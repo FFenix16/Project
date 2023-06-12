d05b9a17c47b7e2fd2e35e628cdcfb30f6cad121
Phoenix <47988088+Faisal-Falah@users.noreply.github.com>
2023-06-13 00:23:40 +0300

    Update TryModule.py

--- SRC/TryModule.py
+++ SRC/TryModule.py
@@ -5,4 +5,4 @@ from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 imgDir = "img/Testmodule/2.png" #Importing the img
 model = YOLO('LOLAIP2.pt')  #Loading the model
-results = model.predict(source=imgDir, save=True, imgsz=320, conf=0.3)
+results = model.predict(source=imgDir, save=True, imgsz=320, conf=0.3) #Run and save the model
